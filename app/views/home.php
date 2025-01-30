9543a03c1efe3723afacb82c3344fa028a0810cb
zwecoder <zwewathone77@gmail.com>
2025-01-30 22:22:21 +0630

    second addition

--- app/views/home.php
+++ app/views/home.php
@@ -2,13 +2,30 @@
 require_once(APPROOT . "/views/inc/header.php");
 require_once(APPROOT . "/views/inc/nav.php");
 ?>
-<div class="py-5" style="background-image: url('bg-image.png');">
+<style>
+
+</style>
+<div class="py-5 bg-image">
     <?= flash('admin_logout'); ?>
     <div class="container mt-5">
         <div class="row">
-            <div class="col-md-12">
-
-
+            <div class="col-md-12 py-5 text-center">
+                <!-- form start -->
+                <div class="box">
+                    <h2>Login</h2>
+                    <form action="">
+                        <div class="inputBox">
+                            <input type="text" name="" required="">
+                            <label for="">Username</label>
+                        </div>
+                        <div class="inputBox">
+                            <input type="password" name="" required="">
+                            <label for="">Password</label>
+                        </div>
+                        <input type="submit" name="" value="Submit">
+                    </form>
+                </div>
+                <!-- form end -->
             </div>
         </div>
     </div>
--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Home page</title>
     <link rel="stylesheet" href="app/resources/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php URLROOT; ?>/app/resources/css/styles.css">
+    <link rel="stylesheet" href="/app/resources/css/custom.css">
 </head>
 
 <body>
